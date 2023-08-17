0fe55cbcbfc1d26567620244e84e03826d86a24b
newwohh <nevoznvx@gmail.com>
2023-08-18 01:01:49 +0530

    heatmap-feature

--- controllers/heatmapController.js
+++ controllers/heatmapController.js
@@ -1,5 +1,15 @@
-exports.heatmap = async (req, res, next) => {
-  const { user } = req.body;
+const Subscription = require("../models/subscriptionModel");
+const catchAsync = require("../utils/catchAsync");
 
-  res.send({ status: "success" });
-};
+exports.heatmap = catchAsync(async (req, res, next) => {
+  const data = await Subscription.find({ user: req.params.id });
+  //   console.log(req.params);
+  const doc = await data;
+
+  res.status(200).json({
+    status: "success",
+    data: data,
+  });
+
+  next();
+});
